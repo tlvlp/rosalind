e5ba6ce239b92ec0a5f093edb99e9ada4a7ba8b7
tlvlp <tllvllp@gmail.com>
2016-10-26 02:29:56 +0200

    Started working on lcsm

--- src/problems/lcsm.java
+++ src/problems/lcsm.java
@@ -26,19 +26,49 @@ public class lcsm {
         //declaring the longest string and giving it the initial value of the first fasta sequence
         String longest = inList.get(1);
         
-        //loop through the longest common sequence and see if the next fasta sequence contains it too 
-        for (int i=1;i) {
-            longest = inList.get(1);
-            String next = inList.get(3);
-            System.out.println(longest);
+        //loop through all the fasta sequences in the output (everey 2nd entry as every 1st is the header)
+        for (int i=3; i<inList.size(); i+=2) {
+            //get the currentSeq sequence
+            String currentSeq = inList.get(i);
+            //collect the longest common subset of the previous longest substring and the next full sequence 
+            longest = getLongest(currentSeq,longest);
+            System.out.println(longest); //debug - print out the longest after checking each sequence
         }
-            //the common subset of longest and next becomes the new longest
-            
-            
-        
         
         // return the longest common substring
         outList.add(longest);
         return outList;
+    }
+    
+    public static String getLongest (String currentSeq, String longest) {
+     /* collects the longest common subset of the previous longest substring and the next full sequence */
+        
+        //list to collect the common sub
+        ArrayList<String> commonList = new ArrayList<>();
+        
+        //loop through all the substring variations of longest
+        for (int i=0; i<longest.length()-1; i++) {
+            for (int e=1; e<longest.length(); e++) {
+                //this substring variation of longest
+                String subOfLongest = longest.substring(i, e);
+                // if this substring of longest is a substring of currentSeq as well it is added to the list 
+                if (isValidSubStr(subOfLongest, currentSeq)) {
+                    commonList.add(subOfLongest);
+                }
+                
+            }
+        }
+        
+        //the elements of commonList are checked and the longest will be chosen
+        /* 
+        !! should be a list if there are more than one !! 
+        fuck then it will have to be done for each element of the new longest list isntead of handling only one string
+        */
+        
+        return longest;
+    }
+    
+    public static boolean isValidSubStr(String checkStr, String baseStr) {
+        //loop through the currentSeq and see if subOfLongest is contained at all - returns true if a match is found, returns false at the end of the loop
     }
 }
